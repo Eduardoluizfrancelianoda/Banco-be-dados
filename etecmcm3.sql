0adf97007063e9089a98093b1d9c9ea21c4a13e6
Eduardo Luiz Franceliano <159642647+Eduardoluizfrancelianoda@users.noreply.github.com>
2025-05-26 13:06:37 -0300

    Add files via upload

--- etecmcm3.sql
+++ etecmcm3.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 05/05/2025 às 14:06
--- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
+-- Tempo de geração: 26/05/2025 às 18:06
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -47,8 +47,7 @@ INSERT INTO `categorias` (`id_categoria`, `nome_cat`) VALUES
 (6, 'congelados'),
 (7, 'eletroeletrodomesticos'),
 (8, 'perfumaria'),
-(9, 'bebidas'),
-(10, 'congelados');
+(9, 'bebidas');
 
 -- --------------------------------------------------------
 
@@ -62,8 +61,33 @@ CREATE TABLE `produtos` (
   `marca` varchar(255) DEFAULT NULL,
   `qtde` int(11) DEFAULT NULL,
   `preco` decimal(10,2) DEFAULT NULL,
-  `id_categoria` int(11) DEFAULT NULL
+  `id_categoria` int(11) DEFAULT NULL,
+  `unidade_medida` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id_produto`, `nome`, `marca`, `qtde`, `preco`, `id_categoria`, `unidade_medida`) VALUES
+(1, 'sabão em pó', 'omo', 4, 0.00, 1, 'peso'),
+(2, 'detergente', 'Ypê', 20, 0.00, 1, 'unidade'),
+(3, 'mussarella', 'Italac', 100, 0.00, 2, 'peso'),
+(4, 'iogurte', 'Danone', 5, 0.00, 3, 'peso'),
+(5, 'pernil', 'Sadia', 10, 0.00, 4, 'peso'),
+(6, 'feijão', 'Kicaldo', 50, 0.00, 5, 'unidade'),
+(7, 'sorvete', 'Kibon', 5, 0.00, 6, 'peso'),
+(8, 'liquidificador', 'phillips', 1, 35.00, 7, 'unidade'),
+(9, 'presunto', 'Sadia', 30, 45.00, 2, 'peso'),
+(10, 'leite', 'Italac', 50, 4.59, 3, 'litro'),
+(11, 'frango', 'Seara', 20, 18.90, 4, 'peso'),
+(12, 'milho enlatado', 'Quero', 40, 3.50, 5, 'unidade'),
+(13, 'lasanha congelada', 'Perdigão', 10, 12.90, 6, 'unidade'),
+(14, 'batedeira', 'Arno', 2, 120.00, 7, 'unidade'),
+(15, 'shampoo', 'Pantene', 15, 14.99, 8, 'unidade'),
+(16, 'desodorante', 'Rexona', 30, 9.99, 8, 'unidade'),
+(17, 'refrigerante', 'Coca-Cola', 25, 7.50, 9, 'litro'),
+(18, 'suco de laranja', 'Del Valle', 20, 6.80, 9, 'litro');
 
 --
 -- Índices para tabelas despejadas
@@ -96,7 +120,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Restrições para tabelas despejadas
